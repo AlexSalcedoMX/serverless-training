6e4fef18c62afa55926c7e67eb23fce8155e2d7d
AlexSalcedoMX <alex.salcedo.gonzalez@gmail.com>
2020-11-02 21:54:35 -0600

    Initial commit

---
+++ src/add_user/handler.js
@@ -0,0 +1,9 @@
+const { generateResponse } = require('../../utils/response')
+
+module.exports.main = async event => {
+  return generateResponse({
+    statusCode: 200,
+    body: {message: 'Add user'}
+  })
+};
+
