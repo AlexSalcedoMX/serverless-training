49edcec01ce5aa25d2299323ff0172037291023d
AlexSalcedoMX <alex.salcedo.gonzalez@gmail.com>
2021-01-18 21:48:20 -0600

    Add filter validation example

--- src/add_user/handler.js
+++ src/add_user/handler.js
@@ -1,9 +1,10 @@
 const { generateResponse } = require('../../utils/response')
 
+ 
 module.exports.main = async event => {
   return generateResponse({
     statusCode: 200,
-    body: {message: 'Add user'}
+    body: { message: 'Add user' }
   })
 };
 
--- src/get_user/handler.js
+++ src/get_user/handler.js
@@ -1,9 +1,19 @@
 const { generateResponse } = require('../../utils/response')
 
-module.exports.main = async event => {
+module.exports.main = async (event) => {
+  console.log(`Event: ${event}`)
+  const { filter } = event.queryStringParamters
+  console.log(`Filter value: ${filter}`)
+  // Filter is required
+  if (!filter) {
+    return generateResponse({
+      statusCode: 400,
+      body: { message: 'Query string parameter is required: filter'}
+    })
+  }
   return generateResponse({
     statusCode: 200,
-    body: {message: 'Get user'}
+    body: { message: 'Get user' }
   })
 };
 
