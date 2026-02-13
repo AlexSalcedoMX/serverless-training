0233411f5579d6683fb54058abffec0febcd4776
Alejandro Salcedo <alex@MacBook-Pro-de-Alejandro.local>
2026-02-13 13:39:59 -0600

    PR 1

--- src/add_user/handler.js
+++ src/add_user/handler.js
@@ -3,7 +3,10 @@ const { generateResponse } = require('../../utils/response')
 module.exports.main = async event => {
   return generateResponse({
     statusCode: 200,
-    body: {message: 'Add user'}
+    body: {
+      message: 'Add user',
+      user: "Alex"
+    }
   })
 };
 
